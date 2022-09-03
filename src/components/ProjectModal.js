2e5c90eec9861809ea64776767ca7561d0d5293e
shingchan95 <chanhei888@gmail.com>
2022-09-03 22:21:37 +0100

    changes in package json

--- src/components/ProjectModal.js
+++ src/components/ProjectModal.js
@@ -30,7 +30,7 @@ export default function ProjectModal ({data, children, open, onClose}) {
       <div style={OVERLAY_STYLES} onClick={onClose} className="cursor-pointer"/>
         <div style={MODAL_STYLES} className="md:w-3/6 md:h-3/5">
             <div className='text-black'>
-              <img src={data.image} className="w-3/6 h-3/5" alt="project"/>
+              <img src= {data.image} className="w-3/6 h-3/5" alt="project"/>
                 <p>{data.title}</p>
                 <p>{data.description}</p>
                 <p>{data.github}</p>
